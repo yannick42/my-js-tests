9abb540c78764356f4351fa4551bac4e3a7d59dd
yannick42 <yannick4283@hotmail.com>
2017-06-23 14:41:53 +0200

    Test : tuiles Touch Events / Mouse Events display:flex

--- PWA/test_1.js
+++ PWA/test_1.js
@@ -12,9 +12,74 @@ function checkAvailability() {
         warnings.push('fetch API not available in this browser');
     }
 
-    var div = document.createElement('div');
+    var span = document.createElement('span');
     var html = (warnings.length ? warnings : ['everything ok']).join('<br/>');
-    div.innerHTML = html;
+    span.innerHTML = html;
     // Display warnings
-    document.body.appendChild(div);
+    document.querySelector('#tile_1 > .tileContent').appendChild(span);
+}
+
+function addUIEvents() {
+
+    for(var i = 1; i <= 6; i++) {
+        addTileEvents(document.getElementById('tile_' + i));
+    }
+
+}
+
+function addTileEvents(tile) {
+
+    var funcStart = function(evt) {
+        evt.preventDefault(); // pour ne pas déclencher l'évenèment souris, et que cette evenements ne soit lancé qu'une fois...
+        var touches = evt.changedTouches;
+        if(touches) {
+            window.myX = touches[0].clientX;
+        }else{
+            window.myX = evt.clientX;
+            document.body.addEventListener("mousemove", funcMove, false);
+        }
+    };
+    var funcCancel = function(evt) {
+        evt.preventDefault();
+        var touches = evt.changedTouches;
+        tile.classList.add('animate');
+        tile.style.transform = 'rotateY(0deg)';
+        document.body.removeEventListener("mousemove", funcMove, false);
+        setTimeout(function() { tile.classList.remove('animate') }, 1000);
+    };
+    var funcMove = function(evt) {
+        evt.preventDefault();
+        var touches = evt.changedTouches;
+        if(touches) {
+            pos = touches[0].clientX;
+        }else{
+            pos = evt.clientX;
+            document.body.addEventListener("mouseup", funcCancel, false);
+        }
+        var deg = ((pos - window.myX)/4)%110;
+        tile.style.transform = 'rotateY('+deg+'deg)';
+    };
+
+    // Touch Events
+    tile.addEventListener('touchstart', funcStart, false);
+    tile.addEventListener("touchend", funcCancel, false);
+    tile.addEventListener("touchcancel", funcCancel, false);
+    tile.addEventListener("touchleave", funcCancel, false);
+    tile.addEventListener("touchmove", funcMove, false); // On bouge le doigt
+
+    // clicks
+    tile.addEventListener('mousedown', funcStart, false);
+}
+
+function writeToTile(tileNumber, className, value) {
+    var tileEl = document.getElementById('tile_'+tileNumber);
+    var content = tileEl.querySelector('.'+className);
+    if(!content) {
+        var span = document.createElement('span');
+        span.className = className;
+        span.innerHTML = value;
+        tileEl.querySelector('.tileContent').appendChild(span);
+    }else{
+        content.innerHTML = value;
+    }
 }
